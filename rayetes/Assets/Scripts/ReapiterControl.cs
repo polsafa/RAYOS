271db44498390f6069291e5c1138865368c4bbed
polsafa <nork_pol@hotmail.es>
2019-10-14 18:44:34 +0200

    add surtidor
    added surtidor, part 2 project

---
+++ rayetes/Assets/Scripts/ReapiterControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReapiterControl : MonoBehaviour
+{
+    private LineRenderer liner;
+
+    public float lineDistance;
+    // Use this for initialization
+    void Start()
+    {
+        liner = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        liner.SetPosition(0, transform.position);
+        liner.SetPosition(1, transform.position + transform.forward * lineDistance);
+
+    }
+}
